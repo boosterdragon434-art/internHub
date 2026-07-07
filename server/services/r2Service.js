44bc346d4807efbcd651380d51397c32bb94f91c
Esakkiappan10 <esakki0720@gmail.com>
2026-07-07 16:57:19 +0530

    96.6%

--- server/services/r2Service.js
+++ server/services/r2Service.js
@@ -62,6 +62,8 @@ const getClient = () => {
         accessKeyId: process.env.R2_ACCESS_KEY_ID,
         secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
       },
+      requestChecksumCalculation: 'WHEN_REQUIRED',
+      responseChecksumValidation: 'WHEN_REQUIRED',
     });
   }
   return s3Client;
